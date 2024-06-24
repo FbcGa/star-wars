4111d82bd312f6d15cfe6dd585917d3c5c9ec4c4
FbcGa <fabricio.alipazaga.pe@gmail.com>
2024-06-23 23:04:27 -0500

    favorite_planet_fixed

--- src/js/component/cardPlanet.jsx
+++ src/js/component/cardPlanet.jsx
@@ -6,7 +6,7 @@ import { Context } from '../store/appContext'
 export const CardPlanet = ({ planet }, { index }) => {
     const uid = planet.url.split("/")[5];
 
-    const { actions } = useContext(Context);
+    const { store, actions } = useContext(Context);
 
     const handleClik = (planet) => {
         actions.getDetails(planet);
@@ -15,6 +15,9 @@ export const CardPlanet = ({ planet }, { index }) => {
     const addFavorite = (planet) => {
         actions.addFavorites(planet);
     }
+    const getFavoriteButtonClass = (name) => {
+        return store.favorites.some((e) => e.name === name) ? 'btn-danger' : 'btn-dark';
+    }
     return (
         <div className="card text-white" style={{ width: "18rem" }}>
             <img src={"https://starwars-visualguide.com/assets/img/planets/" + (Number(uid) + 2) + ".jpg"} className="card-img-top" alt="..." />
@@ -22,7 +25,8 @@ export const CardPlanet = ({ planet }, { index }) => {
                 <h5 className="card-title">{planet.name}</h5>
                 <div className='d-flex align-items-center justify-content-between'>
                     <Link to={`/character/${uid}`} ><button className="btn btn-primary" onClick={() => handleClik(planet)}>Learn more</button> </Link>
-                    <button className="btn btn-dark btn-lg invisible" onClick={() => addFavorite(planet)}><i className="far fa-heart visible"></i></button>
+                    <button className={`btn ${getFavoriteButtonClass(planet.name)} btn-lg`}
+                        onClick={() => addFavorite(planet)}><i className="far fa-heart"></i></button>
                 </div>
 
             </div>
